277b26c42c1a47bdd854176d9cdda1048c297a7e
elcioabrahao <elcioabrahao@usp.br>
2019-11-26 10:53:05 -0300

    Sort hero list descending and drop last hero.

--- app/src/main/java/com/trantordev/superheroapp/data/model/SuperHeroDataResponse.kt
+++ app/src/main/java/com/trantordev/superheroapp/data/model/SuperHeroDataResponse.kt
@@ -3,5 +3,5 @@ package com.trantordev.superheroapp.data.model
 import com.google.gson.annotations.SerializedName
 
 class SuperHeroDataResponse {
-    @SerializedName("Heroes") val heroes : List<Hero> = ArrayList()
+    @SerializedName("Heroes") var heroes : List<Hero> = ArrayList()
 }
--- app/src/main/java/com/trantordev/superheroapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/trantordev/superheroapp/viewmodel/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.trantordev.superheroapp.data.MainRepository
+import com.trantordev.superheroapp.data.model.Hero
 import com.trantordev.superheroapp.ui.MainListener
 import kotlinx.coroutines.Dispatchers
 
@@ -18,11 +19,22 @@ class MainViewModel internal constructor(
 
     val heroes = liveData(Dispatchers.IO) {
         val retrivedHeroes = mainRepository.getHeroes(resource)
+        val list: List<Hero> = retrivedHeroes.heroes.sortedWith(compareBy({it.score})).sortedByDescending { it.score }.dropLast(1)
+        retrivedHeroes.heroes = list
         emit(retrivedHeroes)
     }
 
     fun onLogoutIconClick(view: View){
         mainListener!!.onClose()
+    }
+
+    fun loadNext(){
+        year += 3
+
+    }
+
+    fun isLoading(){
+
     }
 
 
